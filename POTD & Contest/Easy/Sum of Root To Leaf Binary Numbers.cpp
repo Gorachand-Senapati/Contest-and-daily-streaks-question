0e3732842faeec2de1426102d172f0251055ea27
Gorachand Senapati <gorachandsenapati8@gmail.com>
2026-02-24 12:32:49 +0530

    potd

---
+++ POTD & Contest/Easy/Sum of Root To Leaf Binary Numbers.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, int curr){
+        if(!root) return 0;//if no root
+         curr = curr* 2+ root->val;//here calculate bin to decimal
+        if(!root->left && !root->right){//if finish
+            return curr;
+        }
+        return dfs(root->left,curr) + dfs(root->right,curr);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root,0);
+    }
+};
